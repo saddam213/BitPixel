d1f04585c9d2f72ca71a3feb25209c1d8b8f404b
sa_ddam213 <sa_ddam213@live.com>
2020-02-22 10:40:06 +1300

    Move Get/Add pixel implementation for  GameController PixelHub

--- DotMatrix/Controllers/GameController.cs
+++ DotMatrix/Controllers/GameController.cs
@@ -15,9 +15,7 @@ namespace DotMatrix.Controllers
 	{
 		public IGameReader GameReader { get; set; }
 		public IUserReader UserReader { get; set; }
-		public IPixelWriter PixelWriter { get; set; }
 		public IPixelReader PixelReader { get; set; }
-		public IThrottleCache ThrottleCache { get; set; }
 
 		public async Task<ActionResult> Index(int gameId)
 		{
@@ -30,90 +28,10 @@ namespace DotMatrix.Controllers
 			});
 		}
 
-
-		[HttpPost]
-		[Authorize]
-		[ValidateAntiForgeryToken]
-		public async Task<ActionResult> AddPixel(AddPixelRequest model)
-		{
-			if (!ModelState.IsValid)
-				return Json(new { Success = false });
-
-			var game = await GameReader.GetGame(model.GameId);
-			if (game == null)
-				return Json(new { Success = false, Message = "Game not found" });
-
-			if (game.Status != GameStatus.Started)
-				return Json(new { Success = false, Message = "Game is not currently active" });
-
-			var userId = User.Identity.GetUserId<int>();
-			var rateLimitResult = await CheckRateLimits(userId, game);
-			if (!string.IsNullOrEmpty(rateLimitResult))
-				return Json(new { Success = false, Message = rateLimitResult });
-
-			var result = await PixelWriter.AddPixel(userId, model);
-			if (!result.Success)
-				return Json(new { Success = false, Message = result.Message });
-
-			return Json(new { Success = true });
-		}
-
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<ActionResult> GetPixel(GetPixelRequest model)
-		{
-			if (!ModelState.IsValid)
-				return Json(new { Success = false }, JsonRequestBehavior.AllowGet);
-
-			var game = await GameReader.GetGame(model.GameId);
-			if (game == null)
-				return Json(new { Success = false, Message = "Game not found" });
-
-			if (User.Identity.IsAuthenticated)
-			{
-				if (game.Status == GameStatus.Started)
-				{
-					var userId = User.Identity.GetUserId<int>();
-					var rateLimitResult = await CheckRateLimits(userId, game);
-					if (!string.IsNullOrEmpty(rateLimitResult))
-						return Json(new { Success = false, Message = rateLimitResult });
-
-					var clickResult = await PixelWriter.AddClick(userId, new AddClickRequest(model.GameId, model.X, model.Y));
-					if (!clickResult.Success)
-						return Json(new { Success = false, Message = clickResult.Message });
-				}
-			}
-
-			var result = await PixelReader.GetPixel(model.GameId, model.X, model.Y);
-			if (result == null)
-				return Json(new { Success = false, Message = "Pixel not found." }, JsonRequestBehavior.AllowGet);
-
-			return Json(new
-			{
-				Success = true,
-				Data = result
-			}, JsonRequestBehavior.AllowGet);
-		}
-
 		[HttpGet]
 		public async Task<ActionResult> GetPixels(int gameId)
 		{
 			return Json(await PixelReader.GetPixels(gameId), JsonRequestBehavior.AllowGet);
-		}
-
-
-		private async Task<string> CheckRateLimits(int userId, GameModel game)
-		{
-			if (!await ThrottleCache.ShouldExcecute(userId, $"GameClicks:{game.Id}", TimeSpan.FromSeconds(1), game.ClicksPerSecond))
-				return $"Maximum {game.ClicksPerSecond} clicks per second";
-
-			if (!await ThrottleCache.ShouldExcecute(userId, "UserClicks", TimeSpan.FromHours(24), Constant.ClicksPerDay))
-				return $"Maximum {Constant.ClicksPerDay} clicks per day";
-
-			if (!await ThrottleCache.ShouldExcecute(Request.GetIPAddress(), "IPAddressClicks", TimeSpan.FromHours(24), Constant.ClicksPerDay))
-				return $"Maximum {Constant.ClicksPerDay} clicks per day (ip)";
-
-			return string.Empty;
 		}
 	}
 }
--- DotMatrix/Hubs/PixelHub.cs
+++ DotMatrix/Hubs/PixelHub.cs
@@ -1,16 +1,30 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 using DotMatrix.Base.Extensions;
+using DotMatrix.Cache.Common;
+using DotMatrix.Common.Game;
+using DotMatrix.Common.Pixel;
+using DotMatrix.Enums;
 using DotMatrix.Hubs;
 using DotMatrix.QueueService.Common;
+using DotMatrix.Helpers;
 
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using System.Security.Claims;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DotMatrix
 {
 	public class PixelHub : Hub
 	{
+		public IGameReader GameReader { get; set; }
+		public IPixelWriter PixelWriter { get; set; }
+		public IPixelReader PixelReader { get; set; }
+		public IThrottleCache ThrottleCache { get; set; }
+
 		[AuthorizeLocal]
 		public Task NotifyPoints(PointsNotification notification)
 		{
@@ -118,22 +132,133 @@ namespace DotMatrix
 		{
 			return Clients.All.OnChatMessage(new
 			{
-				Sender = Context.GetUserHandle(),
+				Sender = Context.GetUserName(),
 				Message = message.Truncate(240)
 			});
 		}
 
+		[Authorize]
+		public async Task<PixelResult> AddPixel(AddPixelRequest model)
+		{
+			var game = await GameReader.GetGame(model.GameId);
+			if (game == null)
+				return new PixelResult(false, "Game not found" );
+
+			if (game.Status != GameStatus.Started)
+				return new PixelResult(false, "Game is not currently active" );
+
+			var userId = Context.GetUserId();
+			var rateLimitResult = await CheckRateLimits(userId, game);
+			if (!string.IsNullOrEmpty(rateLimitResult))
+				return new PixelResult(false, rateLimitResult );
+
+			var result = await PixelWriter.AddPixel(userId, model);
+			if (!result.Success)
+				return new PixelResult(false, result.Message );
+
+			return new PixelResult(result);
+		}
+
+		public async Task<PixelResult> GetPixel(GetPixelRequest model)
+		{
+			var game = await GameReader.GetGame(model.GameId);
+			if (game == null)
+				return new PixelResult(false, "Game not found");
+
+			if (Context.IsAuthenticated())
+			{
+				if (game.Status == GameStatus.Started)
+				{
+					var userId = Context.GetUserId();
+					var rateLimitResult = await CheckRateLimits(userId, game);
+					if (!string.IsNullOrEmpty(rateLimitResult))
+						return new PixelResult(false, rateLimitResult);
+
+					var clickResult = await PixelWriter.AddClick(userId, new AddClickRequest(model.GameId, model.X, model.Y));
+					if (!clickResult.Success)
+						return new PixelResult(false, clickResult.Message);
+				}
+			}
+
+			var result = await PixelReader.GetPixel(model.GameId, model.X, model.Y);
+			if (result == null)
+				return new PixelResult(false, "Pixel not found.");
+
+			return new PixelResult(result);
+		}
 
 
+
+		private async Task<string> CheckRateLimits(int userId, GameModel game)
+		{
+			if (!await ThrottleCache.ShouldExcecute(userId, $"GameClicks:{game.Id}", TimeSpan.FromSeconds(1), game.ClicksPerSecond))
+				return $"Maximum {game.ClicksPerSecond} clicks per second";
+
+			if (!await ThrottleCache.ShouldExcecute(userId, "UserClicks", TimeSpan.FromHours(24), Constant.ClicksPerDay))
+				return $"Maximum {Constant.ClicksPerDay} clicks per day";
+
+			if (!await ThrottleCache.ShouldExcecute(Context.GetIPAddress(), "IPAddressClicks", TimeSpan.FromHours(24), Constant.ClicksPerDay))
+				return $"Maximum {Constant.ClicksPerDay} clicks per day (ip)";
+
+			return string.Empty;
+		}
 	}
+
+
+	public class PixelResult
+	{
+		public PixelResult() { }
+		public PixelResult(bool success, string message)
+		{
+			Success = success;
+			Message = message;
+		}
+		public PixelResult(object data)
+		{
+			Success = true;
+			Data = data;
+		}
+		public bool Success { get; set; }
+		public string Message { get; set; }
+		public object Data { get; set; }
+	}
+
+
 
 	public static class HubExtensions
 	{
-		public static string GetUserHandle(this HubCallerContext context)
+		public static string GetUserName(this HubCallerContext context)
 		{
-			return context.User.Identity.IsAuthenticated
-				? context.Request.GetHttpContext().User.Identity.Name
-				: "Guest";
+			if (!(context.User.Identity is ClaimsIdentity principal))
+				throw new UnauthorizedAccessException("Invalid ClaimsIdentity");
+
+			var nameIdentifier = principal.Claims.GetClaimValueAsString(ClaimTypes.Name);
+			if (nameIdentifier == null)
+				throw new UnauthorizedAccessException("NameIdentifier not found");
+
+			return nameIdentifier;
+		}
+
+		public static int GetUserId(this HubCallerContext context)
+		{
+			if (!(context.User.Identity is ClaimsIdentity principal))
+				throw new UnauthorizedAccessException("Invalid ClaimsIdentity");
+
+			var nameIdentifier = principal.Claims.GetClaimValueAsInt(ClaimTypes.NameIdentifier);
+			if (nameIdentifier == null)
+				throw new UnauthorizedAccessException("NameIdentifier not found");
+
+			return nameIdentifier.Value;
+		}
+
+		public static bool IsAuthenticated(this HubCallerContext context)
+		{
+			return context.User.Identity.IsAuthenticated;
+		}
+
+		public static string GetIPAddress(this HubCallerContext context)
+		{
+			return context.Request.GetHttpContext().Request.GetIPAddress();
 		}
 
 		public static dynamic User(this IHubConnectionContext<dynamic> client, int userId)
@@ -144,6 +269,79 @@ namespace DotMatrix
 		public static dynamic Group(this IHubConnectionContext<dynamic> client, int groupId)
 		{
 			return client.Group(groupId.ToString());
+		}
+
+
+
+		public static Claim GetClaim(this IEnumerable<Claim> claims, string name)
+		{
+			if (claims != null && claims.Any())
+				return claims.First(x => x.Type == name);
+
+			return null;
+		}
+
+		public static string GetClaimValueAsString(this IEnumerable<Claim> claims, string name)
+		{
+			if (claims != null && claims.Any())
+				return claims.First(x => x.Type == name).Value;
+
+			return string.Empty;
+		}
+
+		public static DateTime? GetClaimValueAsDateTime(this IEnumerable<Claim> claims, string name)
+		{
+			if (claims != null && claims.Any())
+				return DateTime.TryParse(claims.First(x => x.Type == name).Value, out DateTime returnValue)
+				? returnValue
+				: default(DateTime?);
+
+			return default(DateTime?);
+		}
+
+		public static int? GetClaimValueAsInt(this IEnumerable<Claim> claims, string name)
+		{
+			if (claims != null && claims.Any())
+				return int.TryParse(claims.First(x => x.Type == name).Value, out int returnValue)
+				? returnValue
+				: default(int?);
+
+			return default(int?);
+		}
+
+		public static bool? GetClaimValueAsBool(this IEnumerable<Claim> claims, string name)
+		{
+			if (claims != null && claims.Any())
+				return bool.TryParse(claims.First(x => x.Type == name).Value, out bool returnValue)
+				? returnValue
+				: default(bool?);
+
+			return default(bool?);
+		}
+
+		public static Guid? GetClaimValueAsGuid(this IEnumerable<Claim> claims, string name)
+		{
+			if (claims != null && claims.Any())
+				return Guid.TryParse(claims.First(x => x.Type == name).Value, out Guid returnValue)
+				? returnValue
+				: default(Guid?);
+
+			return default(Guid?);
+		}
+
+		public static T? GetClaimValueAsEnum<T>(this IEnumerable<Claim> claims, string name) where T : struct
+		{
+			if (claims != null && claims.Any())
+				return Enum.TryParse(claims.First(x => x.Type == name).Value, out T returnValue)
+				? returnValue
+				: default(T?);
+
+			return default(T?);
+		}
+
+		public static bool IsAuthenticated(this ClaimsPrincipal principal)
+		{
+			return principal?.Identity != null && principal.Identity.IsAuthenticated;
 		}
 	}
 }
